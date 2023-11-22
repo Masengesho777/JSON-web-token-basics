4bdc19879fa32359b8b3da4e91742bce548799ec
masengeshocadeau <masengeshocadeau603@gmail.com>
2023-11-22 11:51:01 +0200

    ends of project

--- controllers/main.js
+++ controllers/main.js
@@ -3,8 +3,8 @@
 // send back to fron-end
 // setup authentication so only the request with JWT can access the dasboard
 
-// const jwt = require('jsonwebtoken')
-// const  customApiError  = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
+const { BadRequestError } = require('../errors/custom-error')
 
 const login = async (req, res) => {
   const { username, password } = req.body
@@ -13,17 +13,17 @@ const login = async (req, res) => {
   // check in the controller
 
   if (!username || !password) {
-    throw new  customApiError('Please provide email and password',400)
+    throw new BadRequestError('Please provide email and password')
   }
 
-  // //just for demo, normally provided by DB!!!!
-  // const id = new Date().getDate()
+  //just for demo, normally provided by DB!!!!
+  const id = new Date().getDate()
 
   // try to keep payload small, better experience for user
   // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
-  // const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-  //   expiresIn: '30d',
-  // })
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
 
   res.status(200).json({ msg: 'user created', token })
 }
